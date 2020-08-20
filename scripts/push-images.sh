fc8e19f00ba2aad59b67c208f3279ea71e257b3f
Tomasz Zaremba <tomasz.zaremba@o2.pl>
2020-08-20 09:16:26 +0200

    Script for pushing Docker images

---
+++ scripts/push-images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker push tomekzar/quarkus-vs-spring-boot-performance:quarkus-jvm
+docker push tomekzar/quarkus-vs-spring-boot-performance:quarkus-native
+docker push tomekzar/quarkus-vs-spring-boot-performance:quarkus-mutiny-jvm
+docker push tomekzar/quarkus-vs-spring-boot-performance:quarkus-mutiny-native
+docker push tomekzar/quarkus-vs-spring-boot-performance:spring-boot-jvm
+docker push tomekzar/quarkus-vs-spring-boot-performance:spring-boot-webflux-jvm
